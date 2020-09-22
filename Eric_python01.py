2cd72c3b65f60dbe1d7a84efdf2db5a429d7ec4a
CobuEric <71341844+CobuEric@users.noreply.github.com>
2020-09-22 15:17:06 -0700

    Add files via upload

---
+++ Eric_python01.py
@@ -0,0 +1,59 @@
+print("Banana")
+print("78-54")
+print("Hello World")
+print("My mane is Buba")
+print("I like to eat mangos")
+print("2"+"2")
+print(2+2)
+print("1"+"1"+"1")
+print(3*3)
+print(type(3))
+print(type(3.5))
+print(type(3.0))
+print(type("hello"))
+d= "dragon"
+print(d)
+a= "milk"
+b= "water"
+c=b
+b=a
+a=c
+print (a)
+print(b)
+h1= "dragon"
+h2= "person"
+h3=h2
+h2=h1
+h1=h3
+print(h1)
+print(h2)
+a="milk"
+b="water"
+c=a
+d=b
+b=c
+a=d
+print(a)
+print(b)
+# Here I have Just Swapped the order of the milk and the water
+print(type(a))
+my_pic='''555uu--:0
+:)'''
+print(my_pic)
+hang_man1='''
+             +-----+
+                   |
+                   | 
+                   |
+                   | 
+                 ====='''
+print(hang_man1)
+
+hang_man2= hang_man1='''
+             +-----+
+             O     |
+                   | 
+                   |
+                   | 
+                 ====='''
+print(hang_man2)
